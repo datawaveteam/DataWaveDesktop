0b89bb274e27ede9bef04f359ba8e774e02ae5ee
Stefan <soccerfanatic1996@gmail.com>
2015-01-17 12:27:23 -0500

    made some more stuff

---
+++ src/DataWaveDesktop.java
@@ -0,0 +1,19 @@
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * Created by Raphael on 1/17/2015.
+ */
+public class DataWaveDesktop {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+}
